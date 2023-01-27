8f9392c0fc9de93481cd7e1342070a4ad92fef1f
alexle-aff <ale@americanfirstfinance.com>
2023-01-27 13:00:15 -0600

    create content module with routing

--- projects/mfe-remote-app/src/app/app-routing.module.ts
+++ projects/mfe-remote-app/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'content', loadChildren: () => import('./content/content.module').then(m => m.ContentModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
